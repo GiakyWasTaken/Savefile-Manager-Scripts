f52c78cfcf29f0e28d7a2aa39db4edc0b979c0e5
Giacomo Santilli <giaky2005santy@gmail.com>
2024-03-13 14:36:15 +0100

    Refactored save file upload script to handle file existence and response validation

--- upload_saves.sh
+++ upload_saves.sh
@@ -3,11 +3,15 @@
 # Source the .env file
 source .env
 
+# API endpoint URL
 store_url=$API_URL"savefile"
 
+# Cycle through each file in the NDS_SAVES_PATH
 for file in "$NDS_SAVES_PATH"/*; do
+    # Check if the file exists
     if [ -f "$file" ]; then
-        response=$(curl -X POST \
+        # Send store file request
+        response=$(curl -s -X POST \
             -H "Authorization: Bearer $API_TOKEN" \
             -F "savefile=@$file" \
             -F "fk_id_game=1" \
@@ -15,15 +19,17 @@ for file in "$NDS_SAVES_PATH"/*; do
     
         # echo "$response"
 
-        if [[ $response == *"Unauthenticated"* ]]; then
-            echo "Unauthenticated"
-            exit 1
-        fi
-
         # Extract the file_name from the response
         file_name=$(echo "$response" | grep -oP '(?<="file_name":")[^"]+')
         echo "$file_name"
-        
+
+        # Check if the response contains the file_name
+        if [[ $file_name == "" ]]; then
+            echo "Failed to upload $file"
+            echo "$response"
+            exit 1
+        fi
+
         # stop for loop
         break
     fi
