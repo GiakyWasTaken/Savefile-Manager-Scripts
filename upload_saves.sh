7bfe02f37cbee0f3ce40a3d9778813f123be768f
Giacomo Santilli <giaky2005santy@gmail.com>
2024-03-14 08:49:19 +0100

    Refactored save file upload script to upload only a single file from argument

--- upload_saves.sh
+++ upload_saves.sh
@@ -6,32 +6,60 @@ source .env
 # API endpoint URL
 store_url=$API_URL"savefile"
 
-# Cycle through each file in the NDS_SAVES_PATH
-for file in "$NDS_SAVES_PATH"/*; do
-    # Check if the file exists
-    if [ -f "$file" ]; then
-        # Send store file request
-        response=$(curl -s -X POST \
-            -H "Authorization: Bearer $API_TOKEN" \
-            -F "savefile=@$file" \
-            -F "fk_id_game=1" \
-            "$store_url")
-    
-        # echo "$response"
+# Check if the file argument is provided
+if [ $# -eq 0 ]; then
+    echo "Please provide a file as an argument."
+    exit 1
+fi
 
-        # Extract the file_name from the response
-        file_name=$(echo "$response" | grep -oP '(?<="file_name":")[^"]+')
-        echo "$file_name"
+# Get the file path from the argument
+file="$1"
 
-        # Check if the response contains the file_name
-        if [[ $file_name == "" ]]; then
-            echo "Failed to upload $file"
-            echo "$response"
+# Check if the file exists
+if [ ! -f "$file" ]; then
+    echo "File $file does not exist."
+    exit 1
+fi
+
+# Parse command line options
+shift
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+        --raw)
+            raw_response=true
+            shift
+            ;;
+        *)
+            echo "Unknown option: $key"
             exit 1
-        fi
-
-        # stop for loop
-        break
-    fi
+            ;;
+    esac
 done
 
+# Send store file request
+response=$(curl -s -X POST \
+    -H "Authorization: Bearer $API_TOKEN" \
+    -F "savefile=@$file" \
+    -F "fk_id_game=1" \
+    "$store_url")
+
+
+# Check if the --raw argument is provided
+if [[ $raw_response == true ]]; then
+    # Print the raw response
+    echo "$response"
+else
+    # Extract the file_name from the response
+    file_name=$(echo "$response" | grep -oP '(?<="file_name":")[^"]+')
+
+    # Check if the response contains the file_name
+    if [[ $file_name == "" ]]; then
+        echo "Failed to upload $file"
+        echo "$response"
+        exit 1
+    fi
+
+    # Print the file_name
+    echo "$file_name"
+fi
